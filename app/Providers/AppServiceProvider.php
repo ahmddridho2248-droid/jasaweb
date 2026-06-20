8f60f7f0bf5ef1d0e3d132ae704a0eafd5e40704
ahmadridhobabai <ahmddridho2248@gmail.com>
2026-06-21 02:54:46 +0700

    PUSING

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate; // <-- PASTIIN TAMBAHIN INI DI ATAS
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Bypass semua policy kalo usernya adalah Super Admin
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
+        });
     }
 }
